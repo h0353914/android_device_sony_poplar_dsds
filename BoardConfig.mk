052f80339ac54274714e9195c6d9bd8e56104b05
j4nn <531585+j4nn@users.noreply.github.com>
2025-07-31 00:48:19 +0200

    poplar_dsds: update for twrp-3.7.1_12-0
    fix decryption by avoiding cloning of properties on early boot
    fix possibility for /system backup in files mode
    build kernel from sources instead of prebuilt one

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_BOOTLOADER_BOARD_NAME := G8342
 
 ### KERNEL
 BOARD_KERNEL_CMDLINE += androidboot.hardware=poplar_dsds
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+TARGET_KERNEL_CONFIG := lineage-msm8998-yoshino-poplar_dsds_defconfig
 
 ### PARTITIONS
 # See also /proc/partitions on the device
